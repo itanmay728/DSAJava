7fe2cff3d62d8ff36c6f3ce6bfd6434dfe579dca
itanmay728 <ktanmay1130@gmail.com>
2024-03-07 00:22:18 +0530

    AssignmentOnLoops

--- Array/Problems/src/TestHackerRank.java
+++ Array/Problems/src/TestHackerRank.java
@@ -1,6 +1,17 @@
-import java.util.Scanner;
+ class Test{
 
-public class TestHackerRank {
-    public static void main(String[] args) {
+    public static void main(String args[]){
+
+        String str1 = "one";
+
+        String str2 = "two";
+
+        System.out.println(str1.concat(str2));
+
+        int ++a = 100;
+
+        System.out.println(++a);
+
     }
+
 }
